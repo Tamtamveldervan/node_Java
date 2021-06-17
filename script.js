6a239e119e98d22cde026768f7c73f4fb453bc95
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-17 11:58:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hallo Winc Academy studenten")
+
+
+//dit is een grote som
+/*
+let multiplication=1230941 * 1823794;
+console.log("multiplication:", multiplication)
+
+let division=637263/53
+console.log("division:", division)
+*/
+
+/*je ziet bovenstaande code niet terug in Node, omdat het uit gecomment is, comment out kan je doen
+omdat je foutjes wil oplossen of nog bezit bent met het testen van de code*/ 
+
+
